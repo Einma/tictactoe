9eae04b4f2ae95a4dc85540eb8c7a61f9e3b129c
Einma <tetbirtaimen0@gmail.com>
2023-01-28 14:17:43 +0100

    add You Won popup change colors fix a bug that cause the delay of ending the game

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const GameState = () => {
   const getBoardState = () => boardState;
 
   const resetBoard = () => boardState = Array(9).fill('');
-  const resetBoardWinner = (winner) => boardState = Array(9).fill(`${winner}`);
+  const resetBoardWinner = (winner) => boardState = Array(9).fill(winner);
 
   return {
     boardUpdate,
@@ -39,15 +39,16 @@ const domElements = () => {
   return {
     cells: document.querySelectorAll('.cell'),
     restartBtn: document.querySelector('#restartBtn'),
+    winnderModel: document.querySelector('.winner-model'),
   }
 }
 
 
 const GameManager = (() => {
   const init = function () {
-    let newGame = GameState();
+    const newGame = GameState();
 
-    let playerTurn = true;
+    const playerTurn = true;
 
     const playerX = Player("X");
 
@@ -55,10 +56,10 @@ const GameManager = (() => {
 
     const domElms = domElements();
 
-    return [newGame, playerTurn, playerX, playerO, domElms];
+    return { newGame, playerTurn, playerX, playerO, domElms };
   }
 
-  let [newGame, playerTurn, playerX, playerO, domElms] = init();
+  let { newGame, playerTurn, playerX, playerO, domElms } = init();
 
   const renderBoard = () => {
     let i = 0;
@@ -73,6 +74,31 @@ const GameManager = (() => {
     newGame.resetBoard()
 
     renderBoard()
+  }
+
+  const checkWinner = () => {
+    const board = newGame.getBoardState();
+    const resetWinner = newGame.resetBoardWinner;
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+      let [a, b, c] = winningCombinations[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        resetWinner(board[a]);
+        console.log(board)
+        toggleModel();
+        populateModel(board[a]);
+        return;
+      }
+      if (!board.includes('')) {
+        populateModel('None');
+        newGame.resetBoard();
+      }
+    }
   }
 
   const play = (e) => {
@@ -96,32 +122,37 @@ const GameManager = (() => {
     }
     renderBoard();
     checkWinner();
-
   }
 
-  const checkWinner = () => {
-    const board = newGame.getBoardState();
-    const winningCombinations = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
-    ];
+  const toggleModel = () => {
+    const model = domElms.winnderModel;
+    model.classList.toggle("hidden");
+  }
 
-    for (let i = 0; i < winningCombinations.length; i++) {
-      const [a, b, c] = winningCombinations[i];
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        newGame.resetBoardWinner(board[a]);
-        return;
-      }
-    }
+  const populateModel = (winner) => {
+    const model = domElms.winnderModel;
+    model.textContent = '';
+    const h3 = document.createElement('h3');
+    h3.textContent = 'The Winner Is:';
+    h3.setAttribute('class', 'winner-model-h3');
 
-    if (!board.includes('')) {
-      newGame.resetBoard();
-    }
+    const h1 = document.createElement('h1');
+    h1.textContent = `${winner}`;
+    h1.setAttribute('class', 'winner-model-h1');
+
+    model.appendChild(h3);
+    model.appendChild(h1);
   }
 
   domElms.cells.forEach(cell => cell.addEventListener('click', play));
 
-  domElms.restartBtn.addEventListener('click', restartBoard)
+  domElms.restartBtn.addEventListener('click', () => {
+    const model = domElms.winnderModel;
+    restartBoard();
+    if (!model.classList.contains('hidden')) {
+      toggleModel();
+    }
+  })
+  // domElms.restartBtn.addEventListener('click', showWinner)
 
 })()
